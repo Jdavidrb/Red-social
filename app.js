cef2146253c0ba77d16b5024bd5dba0fb32325e0
Jdavidrb <u.jdavidrb@gmail.com>
2024-08-22 13:39:28 -0500

    Run local host correctly

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path));
 
-// Emdpoint principal
+// Endpoint principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
@@ -17,6 +16,4 @@ app.get('/about', (req, res) => {
 
 app.listen(5000, () => {
     console.log('Server running on port 5000');
-
 });
-
