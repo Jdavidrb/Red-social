2524eeb48d683a106b0c6a41118eb0bffcc7f2f3
Jdavidrb <u.jdavidrb@gmail.com>
2024-09-16 18:24:34 -0500

    cambios en el css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import './Header.css';
+import logoImage from '../../assets/images/logo.png';
+import bellIcon from '../../assets/images/bell-icon.png';
+import searchIcon from '../../assets/images/search-icon.png';
+import profileImage from '../../assets/images/profile-image.png';
 
 function Header() {
   return (
     <header>
-      <h1>Logo</h1>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Explore</li>
-          <li>Chat</li>
-          <li>Profile</li>
-        </ul>
-      </nav>
+      <div className="logo">
+        <img src={logoImage} alt="Logo" />
+        <span>Red Social</span>
+      </div>
+      <div className="actions">
+        <button>
+          <img src={bellIcon} alt="Notificaciones" />
+        </button>
+        <button>
+          <img src={searchIcon} alt="Buscar" />
+        </button>
+        <img src={profileImage} alt="Perfil" className="profile-img" />
+      </div>
     </header>
   );
 }
