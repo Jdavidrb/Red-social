550a9ffc4be8b62885e94dbeeda8bb6c9722deec
Jdavidrb <u.jdavidrb@gmail.com>
2024-09-18 23:37:36 -0500

    adjustments in the home page CSS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,24 +8,14 @@ import img from '../../assets/images/bell-icon.png'; // Importa las imágenes
 function Home() {
   return (
     <div className='Home'>
-      <div className='content-home'>
+      <h1 className='title'>My projects</h1>
+      <div className='my-projects'>
+        
         <Card 
             title="Project Alpha" 
             members={5} 
             dueDate="2 days" 
             image= {img} 
-          />
-          <Card 
-            title="Project Beta" 
-            members={3} 
-            dueDate="1 week" 
-            image= {img}
-          />
-          <Card 
-            title="Project Beta" 
-            members={3} 
-            dueDate="1 week" 
-            image= {img}
           />
           <Card 
             title="Project Beta" 
